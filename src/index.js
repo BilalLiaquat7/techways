2f43de8d9da412649f5c5730f18565bebbd041a7
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2021-02-17 15:31:35 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const app = express()
+
+const staticPath = path.join(__dirname, "../public");
+// console.log(path.join(__dirname, "../public"));
+app.use(express.static(staticPath))
+
+
+//to set engine for dynamic content
+app.set("view engine", "hbs");
+
+
+
+app.get("/", (req,res) => {
+    // res.send("Hello World from express");
+    res.render("index", {
+        Myname: "Mughal Tech",
+    });
+});
+
+app.get("/about", (req,res) => {
+    res.send("Welcome to About Page");
+});
+
+app.get("/contact", (req,res) => {
+    res.send("Welcome to About Page");
+});
+
+app.get("/temp", (req,res) => {
+    res.send({
+        id: 1,
+        name: "Bilal"
+    }
+    );
+});
+
+app.listen(8000, () => {
+    console.log("listening port 8000");
+});
