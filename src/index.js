cf0546208d97c8430b3d18ddaae9621d0c005666
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2021-02-22 22:29:34 +0500

    partials added for about page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ const path = require("path");
 const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express()
+const hbs = require("hbs");
 
 const staticPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../partials");
 // console.log(path.join(__dirname, "../public"));
 app.use(express.static(staticPath))
 
 
 //to set engine for dynamic content
 app.set("view engine", "hbs");
-
-
+hbs.registerPartials(partialsPath);
 
 app.get("/", (req,res) => {
     // res.send("Hello World from express");
@@ -21,11 +22,11 @@ app.get("/", (req,res) => {
 });
 
 app.get("/about", (req,res) => {
-    res.send("Welcome to About Page");
+    res.render("about");
 });
 
 app.get("/contact", (req,res) => {
-    res.send("Welcome to About Page");
+    res.send("Welcome Contact page");
 });
 
 app.get("/temp", (req,res) => {
