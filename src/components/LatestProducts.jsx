6bee1c58188857b7930bb085e62584eec0f794e2
sheezu900 <shahzamanlive2@gmail.com>
2023-03-26 02:00:23 +0500

    ...

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -3,7 +3,7 @@ export function LatestProducts() {
     <div className="bg-white w-4/5 my-14 flex flex-col">
       <h1 className="text-4xl font-bold text-blue-900 flex justify-center">Latest Products</h1>
       <div className="">
-        
+
       </div>
     </div>
   );
