d60e48fa98c3b363d5615c11ab15cb6b2522f21d
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-03-25 07:29:12 +0500

    navbar done

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,7 +1,17 @@
 export function Topbar() {
   return (
     <>
-      <nav></nav>
+      <nav className="bg-gray-100 text-white flex justify-between">
+        <span className="text-xl flex items-center mx-3 font-bold text-gray-700">
+          Techways
+        </span>
+        <ul className="px-28 py-6 flex space-x-3 justify-end">
+          <li className="cursor-pointer text-gray-700">Home</li>
+          <li className="cursor-pointer text-gray-700">About</li>
+          <li className="cursor-pointer text-gray-700">Catalogue</li>
+          <li className="cursor-pointer text-gray-700">Contact Us</li>
+        </ul>
+      </nav>
     </>
   );
 }
